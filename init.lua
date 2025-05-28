6e86763dc15e2d090fde7baf83460a1d4839345c
Nomarian <nomarian@outlook.com>
2025-05-28 13:41:32 -0400

    Only performs modifications if there is a modification to perform

--- init.lua
+++ init.lua
@@ -13,10 +13,15 @@ Notes:
 local M = {}
 
 M.F = function (file)
-	local lines = file.lines
-	for i=1, #lines do
-		local t,c = lines[i]:gsub("%s+$","")
-		if c>0 then lines[i] = t end
+	local lines = file.lines do
+		local t,c = lines[#lines]:gsub("%s+$","")
+		if c>0 then lines[#lines] = t end
+	end
+	if file:content(0,file.size):find"[ \t]\n" then
+		for i=1, #lines do
+			local t,c = lines[i]:gsub("%s+$","")
+			if c>0 then lines[i] = t end
+		end
 	end
 	return true
 end
